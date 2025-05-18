63be770293f01ac91a07e4a87c0c4aa00f03fb79
Noah <noah.martens@student.kuleuven.be>
2025-05-18 18:49:28 +0200

    step 3

---
+++ Step 3/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Function to handle termination signals
+term_handler() {
+  echo "Received termination signal. Exiting..."
+  exit 0
+}
+
+# Trap termination signals
+trap 'term_handler' INT TERM
+
+/app/generate_hashed_users
+
+cat /app/hashed_users.txt
+
+while true; do
+  /app/login
+done
